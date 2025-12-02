fede3367d5e8f0ee4efb929130d7069eb2a4977c
Max <kalmro4@gmail.com>
2025-12-02 14:20:09 +0700

    Rework frontend lint configuration

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2022: true,
+    node: true,
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.json",
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ["@typescript-eslint", "react", "react-hooks", "@next/next"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:@next/next/core-web-vitals",
+  ],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
